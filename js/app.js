70d835e555fa12e79e304335be07da764c961a17
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-06-25 20:26:27 -0300

    Criando a função handleGameWindow

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var height = 0;
+var width = 0;
+//Recovery height:  and width
+function handleGameWindow(){
+  height = window.innerHeight;
+  width = window.innerWidth; 
+
+  console.log(height, width);
+}
